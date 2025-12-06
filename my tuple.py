4a9d73f038ff7c334f2bf9d16856a87f84e8c086
ghosharundhati89-ui <ghosharundhati89@gmail.com>
2025-12-06 12:10:14 +0600

    Add files via upload

---
+++ my tuple.py
@@ -0,0 +1,14 @@
+my_tuple =()
+print(my_tuple)
+#tuple having integers
+my_tuple=(1,2,3)
+print(my_tuple)
+#tuple with mixed datatypes
+my_tuple=("mouse",[8,4,6],(1,2,3))
+print(my_tuple)
+#nested index 
+print(my_tuple[0][3])
+print(my_tuple[1][1])
+#slicing
+print("sliced:",my_tuple[1:4])
+
